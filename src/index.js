2c53a860a44b15f8f51dd9692a77904409d29218
Matthias Le Brun <mlbli@me.com>
2015-10-21 18:45:35 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const mediaQueries = {
+  maxS: "(max-width: 30em)",
+  minS: "(min-width: 30.01em)",
+
+  maxM: "(max-width: 50em)",
+  minM: "(min-width: 50.01em)",
+
+  maxL: "(max-width: 65em)",
+  minL: "(min-width: 65.01em)",
+
+  maxXL: "(max-width: 80em)",
+  minXL: "(min-width: 80.01em)",
+}
+
+export default mediaQueries
